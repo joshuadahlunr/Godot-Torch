a62d8c770018acddf5501a74a425556331f75180
Joshua Dahl <joshuadahl@nevada.unr.edu>
2023-03-30 15:34:05 -0700

    Initial Commit

---
+++ src/GDExtensionTemplate.h
@@ -0,0 +1,16 @@
+#pragma once
+// SPDX-License-Identifier: Unlicense
+#include "godot_cpp/classes/object.hpp"
+
+namespace godot {
+	class ClassDB;
+};
+
+class GDExtensionTemplate : public godot::Object {
+	GDCLASS(GDExtensionTemplate, godot::Object)
+public: 
+    static godot::String version();
+
+private:
+	static void _bind_methods();
+};
